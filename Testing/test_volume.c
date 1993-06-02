c6318cb1e0d3ac31f47d14222ab3a0421b15787a
david <david>
1993-06-02 13:36:19 +0000

    *** empty log message ***

--- Testing/test_volume.c
+++ Testing/test_volume.c
@@ -5,7 +5,13 @@ int  main(
     char  *argv[] )
 {
     Status         status;
+    int            n_alloced, x_size, y_size, i;
+    Real           intensity;
+    pixels_struct  pixels;
     volume_struct  volume;
+    window_struct  *window;
+    Real           scale;
+    Colour         *rgb_map;
     char           *filename;
 
     initialize_argument_processing( argc, argv );
@@ -20,6 +26,38 @@ int  main(
         print( "Thickness: %g %g %g\n",
                 volume.thickness[X], volume.thickness[Y], volume.thickness[Z] );
     }
+
+    status = G_create_window( "Volume Browser", -1, -1, -1, -1, &window );
+
+    G_get_window_size( window, &x_size, &y_size );
+
+    n_alloced = 0;
+
+    ALLOC( rgb_map, volume.max_value+1 );
+
+    for_less( i, 0, volume.max_value+1 )
+    {
+        intensity = (Real) i / (Real) volume.max_value;
+        rgb_map[i] = make_Colour_0_1( intensity, intensity, intensity );
+    }
+
+    scale = (Real) x_size / (Real) volume.sizes[X];
+    if( (Real) y_size / (Real) volume.sizes[Y] < scale )
+        scale = (Real) y_size / (Real) volume.sizes[Y];
+
+    create_volume_slice( &volume, (Real) volume.sizes[Z] / 2.0,
+                         0.0, 0.0, scale, scale,
+                         (volume_struct *) NULL, 0.0, 0.0, 0.0, 0.0, 0.0,
+                         X, Y, x_size, y_size, RGB_PIXEL, FALSE,
+                         (unsigned short **) NULL,
+                         &rgb_map, &n_alloced, &pixels );
+
+    G_set_view_type( window, PIXEL_VIEW );
+    G_draw_pixels( window, &pixels );
+    G_update_window( window );
+
+    print( "Hit return: " );
+    (void) getchar();
 
     return( status != OK );
 }
