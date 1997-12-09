8912b1e924f0a0a327dad5d61bc12310e28e98f6
david <david>
1997-12-09 15:24:16 +0000

    *** empty log message ***

--- GLUT_windows/glut_windows.c
+++ GLUT_windows/glut_windows.c
@@ -1,11 +1,11 @@
 #include  <internal_volume_io.h>
 #include  <WS_graphics.h>
 
-private  void  initialize_glut( void )
+public  void  WS_initialize( void )
 {
     static  BOOLEAN  initialized = FALSE;
-    int              argc = 0;
-    char             **argv = NULL;
+    int              argc = 1;
+    char             *argv[] = { "no_program_name" };
 
     if( !initialized )
     {
@@ -163,9 +163,9 @@ public  void  WS_set_colour_map_entry(
     int               ind,
     Colour            colour )
 {
-    glutSetColor( ind, get_Colour_r_0_1(colour),
-                       get_Colour_g_0_1(colour),
-                       get_Colour_b_0_1(colour) );
+    glutSetColor( ind, (float) get_Colour_r_0_1(colour),
+                       (float) get_Colour_g_0_1(colour),
+                       (float) get_Colour_b_0_1(colour) );
 }
 
 public  void  WS_set_overlay_colour_map_entry(
@@ -253,18 +253,18 @@ public  void  WS_draw_text(
     font = lookup_font( type, size, NULL );
 
     for_less( i, 0, string_length( string ) )
-        glutBitmapCharacter( font, string[i] );
+        glutBitmapCharacter( font, (int) string[i] );
 }
 
 public  Real  WS_get_character_height(
     Font_types       type,
     Real             size )
 {
-    int   which, height;
+    int   height;
 
     (void) lookup_font( type, size, &height );
 
-    return( height );
+    return( (Real) height );
 }
 
 public  Real  WS_get_text_length(
@@ -279,7 +279,7 @@ public  Real  WS_get_text_length(
 
     len = 0;
     for_less( i, 0, (int) strlen( str ) )
-        len += glutBitmapWidth( font, str[i] );
+        len += glutBitmapWidth( font, (int) str[i] );
 
     return( (Real) len );
 }
@@ -292,13 +292,272 @@ public  void  WS_get_screen_size(
     *y_size = glutGet( (GLenum) GLUT_SCREEN_HEIGHT );
 }
 
+static  void  (*display_callback) ( Window_id );
+static  void  (*display_overlay_callback) ( Window_id );
+static  void  (*resize_callback) ( Window_id, int, int, int, int );
+static  void  (*key_down_callback) ( Window_id, int, int, int, int );
+static  void  (*key_up_callback) ( Window_id, int, int, int, int );
+static  void  (*mouse_motion_callback) ( Window_id, int, int );
+static  void  (*left_down_callback) ( Window_id, int, int, int );
+static  void  (*left_up_callback) ( Window_id, int, int, int );
+static  void  (*middle_down_callback) ( Window_id, int, int, int );
+static  void  (*middle_up_callback) ( Window_id, int, int, int );
+static  void  (*right_down_callback) ( Window_id, int, int, int );
+static  void  (*right_up_callback) ( Window_id, int, int, int );
+static  void  (*iconify_callback) ( Window_id );
+static  void  (*deiconify_callback) ( Window_id );
+static  void  (*enter_callback) ( Window_id );
+static  void  (*leave_callback) ( Window_id );
+
 public  void  WS_set_update_function(
-    void  (*func)( void ) )
+    void  (*func)( Window_id ) )
 {
-    glutDisplayFunc( func );
+    display_callback = func;
+}
+
+public  void  WS_set_update_overlay_function(
+    void  (*func)( Window_id ) )
+{
+    display_overlay_callback = func;
+}
+
+public  void  WS_set_resize_function(
+    void  (*func)( Window_id, int, int, int, int ) )
+{
+    resize_callback = func;
+}
+
+public  void  WS_set_key_down_function(
+    void  (*func)( Window_id, int, int, int, int ) )
+{
+    key_down_callback = func;
+}
+
+public  void  WS_set_key_up_function(
+    void  (*func)( Window_id, int, int, int, int ) )
+{
+    key_up_callback = func;
+}
+
+public  void  WS_set_mouse_movement_function(
+    void  (*func)( Window_id, int, int ) )
+{
+    mouse_motion_callback = func;
+}
+
+public  void  WS_set_left_mouse_down_function(
+    void  (*func)( Window_id, int, int, int ) )
+{
+    left_down_callback = func;
+}
+
+public  void  WS_set_left_mouse_up_function(
+    void  (*func)( Window_id, int, int, int ) )
+{
+    left_up_callback = func;
+}
+
+public  void  WS_set_middle_mouse_down_function(
+    void  (*func)( Window_id, int, int, int ) )
+{
+    middle_down_callback = func;
+}
+
+public  void  WS_set_middle_mouse_up_function(
+    void  (*func)( Window_id, int, int, int ) )
+{
+    middle_up_callback = func;
+}
+
+public  void  WS_set_right_mouse_down_function(
+    void  (*func)( Window_id, int, int, int ) )
+{
+    right_down_callback = func;
+}
+
+public  void  WS_set_right_mouse_up_function(
+    void  (*func)( Window_id, int, int, int ) )
+{
+    right_up_callback = func;
+}
+
+public  void  WS_set_iconify_function(
+    void  (*func)( Window_id ) )
+{
+    iconify_callback = func;
+}
+
+public  void  WS_set_deiconify_function(
+    void  (*func)( Window_id ) )
+{
+    deiconify_callback = func;
+}
+
+public  void  WS_set_enter_function(
+    void  (*func)( Window_id ) )
+{
+    enter_callback = func;
+}
+
+public  void  WS_set_leave_function(
+    void  (*func)( Window_id ) )
+{
+    leave_callback = func;
+}
+
+private  int  get_keyboard_modifiers( void )
+{
+    int  modifier, glut_mod;
+
+    modifier = 0;
+    glut_mod = glutGetModifiers();
+
+    if( (glut_mod & GLUT_ACTIVE_SHIFT) != 0 )
+        modifier |= SHIFT_KEY_BIT;
+    if( (glut_mod & GLUT_ACTIVE_CTRL) != 0 )
+        modifier |= CTRL_KEY_BIT;
+    if( (glut_mod & GLUT_ACTIVE_ALT) != 0 )
+        modifier |= ALT_KEY_BIT;
+
+    return( modifier );
+}
+
+private  void  display_function( void )
+{
+    (*display_callback) ( WS_get_current_window_id() );
+}
+
+private  void  display_overlay_function( void )
+{
+    (*display_overlay_callback) ( WS_get_current_window_id() );
+}
+
+private  void  resize_function(
+    int   width,
+    int   height )
+{
+    int   x, y;
+
+    x = glutGet( (GLenum) GLUT_WINDOW_X );
+    y = glutGet( (GLenum) GLUT_WINDOW_Y );
+
+    (*resize_callback) ( WS_get_current_window_id(), x, y, width, height );
+}
+
+private  void  keyboard_function(
+    unsigned  char  key,
+    int             x,
+    int             y )
+{
+    (*key_down_callback) ( WS_get_current_window_id(), (int) key, x, y,
+                           get_keyboard_modifiers() );
+}
+
+private  void  special_keyboard_function(
+    int     key,
+    int     x,
+    int     y )
+{
+    int   translated;
+
+    translated = -1000;
+
+    switch( key )
+    {
+    case GLUT_KEY_LEFT:     translated = LEFT_ARROW_KEY;  break;
+    case GLUT_KEY_RIGHT:    translated = RIGHT_ARROW_KEY;  break;
+    case GLUT_KEY_UP:       translated = UP_ARROW_KEY;  break;
+    case GLUT_KEY_DOWN:     translated = DOWN_ARROW_KEY;  break;
+    }
+
+    if( translated != -1000 )
+        (*key_down_callback) ( WS_get_current_window_id(), translated, x, y,
+                               get_keyboard_modifiers() );
+}
+
+private  void  mouse_button_function(
+    int     button,
+    int     state,
+    int     x,
+    int     y )
+{
+    int         modifiers;
+    Window_id   window_id;
+
+    window_id = WS_get_current_window_id();
+    modifiers = get_keyboard_modifiers();
+
+    switch( button )
+    {
+    case GLUT_LEFT_BUTTON:
+        if( state == GLUT_DOWN )
+            (*left_down_callback) ( window_id, x, y, modifiers );
+        else
+            (*left_up_callback) ( window_id, x, y, modifiers );
+        break;
+
+    case GLUT_MIDDLE_BUTTON:
+        if( state == GLUT_DOWN )
+            (*middle_down_callback) ( window_id, x, y, modifiers );
+        else
+            (*middle_up_callback) ( window_id, x, y, modifiers );
+        break;
+
+    case GLUT_RIGHT_BUTTON:
+        if( state == GLUT_DOWN )
+            (*right_down_callback) ( window_id, x, y, modifiers );
+        else
+            (*right_up_callback) ( window_id, x, y, modifiers );
+        break;
+    }
+}
+
+private  void  mouse_motion_function(
+    int     x,
+    int     y )
+{
+    (*mouse_motion_callback) ( WS_get_current_window_id(), x, y );
+}
+
+private  void  entry_function(
+    int     state )
+{
+    Window_id   window_id;
+
+    window_id = WS_get_current_window_id();
+
+    if( state == GLUT_LEFT )
+        (*leave_callback) ( window_id );
+    else
+        (*enter_callback) ( window_id );
 }
 
 public  void  WS_event_loop( void )
 {
+    glutDisplayFunc( display_function );
+    glutOverlayDisplayFunc( display_overlay_function );
+    glutReshapeFunc( resize_function );
+    glutKeyboardFunc( keyboard_function );
+    glutSpecialFunc( special_keyboard_function );
+    glutMouseFunc( mouse_button_function );
+    glutMotionFunc( mouse_motion_function );
+    glutPassiveMotionFunc( mouse_motion_function );
+    glutEntryFunc( entry_function );
+
     glutMainLoop();
 }
+
+public  void  WS_set_update_flag(
+    WSwindow   window  )
+{
+    Window_id  save_id;
+
+    save_id = glutGetWindow();
+
+    set_window_normal_planes( window );
+
+    glutPostRedisplay();
+
+    glutSetWindow( save_id );
+}
+
