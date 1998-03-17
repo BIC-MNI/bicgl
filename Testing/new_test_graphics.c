a65d18d4b95f467796dd4c72cff6190bf38faa08
david <david>
1998-03-17 16:13:30 +0000

    *** empty log message ***

--- Testing/new_test_graphics.c
+++ Testing/new_test_graphics.c
@@ -4,9 +4,6 @@
 #include  <GS_graphics.h>
 #include  <WS_graphics.h>
 
-#define  DEFAULT_WINDOW_X_SIZE    300
-#define  DEFAULT_WINDOW_Y_SIZE    300
-
 private  void  display_function( void )
 {
     print( "Display callback\n" );
@@ -18,20 +15,19 @@ private  void  create_GLUT_window(
     int                    initial_y_pos,
     int                    initial_x_size,
     int                    initial_y_size,
-    BOOLEAN                colour_map_mode )
+    BOOLEAN                rgb_mode )
 {
     int                rgba;
     unsigned  int      mode;
     int                used_size;
-    Window_id          window_id;
-    BOOLEAN            actual_colour_map_mode;
+    int                window_id;
 
     mode = 0;
 
-    if( colour_map_mode )
-        mode |= GLUT_INDEX;
-    else
+    if( rgb_mode )
         mode |= GLUT_RGB;
+    else
+        mode |= GLUT_INDEX;
 
     mode |= GLUT_DOUBLE;
     mode |= GLUT_DEPTH;
@@ -52,8 +48,8 @@ private  void  create_GLUT_window(
 
     if( !glutGet( (GLenum) GLUT_DISPLAY_MODE_POSSIBLE ) )
     {
-        print_error( "Could not open GLUT window in Display mode (%d,%d) for OpenGL\n",
-                     colour_map_mode, TRUE );
+        print_error( "Could not open GLUT window in rgb mode=%d for OpenGL\n",
+                     rgb_mode );
         return;
     }
 
@@ -73,13 +69,11 @@ private  void  create_GLUT_window(
 
     glutDisplayFunc( display_function );
 
-    actual_colour_map_mode = !rgba;
-
-    if( actual_colour_map_mode != colour_map_mode )
+    if( rgba && !rgb_mode || !rgba && rgb_mode )
     {
-        print_error( "Could not get requested colour_map_mode(%d), got(%d,%d)\n",
-                     colour_map_mode,
-                     !glutGet( (GLenum) GLUT_WINDOW_RGBA ),
+        print_error( "Could not get requested rgb_mode(%d), got(%d,%d)\n",
+                     rgb_mode,
+                     glutGet( (GLenum) GLUT_WINDOW_RGBA ),
                      glutGet( (GLenum) GLUT_WINDOW_COLORMAP_SIZE ) );
 
     }
@@ -87,41 +81,14 @@ private  void  create_GLUT_window(
     return;
 }
 
-private  void  G_create(
-    STRING         title,
-    int            x_pos,
-    int            y_pos,
-    int            width,
-    int            height,
-    BOOLEAN        colour_map_desired )
-{
-    create_GLUT_window( title, x_pos, y_pos, width, height,
-                        colour_map_desired );
-}
-
 int main(
     int    argc,
     char   *argv[] )
 {
-#ifndef OLD
-    Gwindow  first, second;
-
     glutInit( &argc, argv );
 
-    G_create( "Test Window", 100, 600, 300, 300, FALSE );
-
-    G_create( "Second Window", 500, 600, 200, 200, TRUE );
-#else
-    Gwindow  first, second;
-
-    ALLOC( first, 1 );
-    ALLOC( first->GS_window, 1 );
-    first->GS_window->WS_window.window_id = create_GLUT_window( "Test Window", 100, 600, 300, 300, FALSE );
-
-    ALLOC( second, 1 );
-    ALLOC( second->GS_window, 1 );
-    second->GS_window->WS_window.window_id = create_GLUT_window( "Second Window", 500, 600, 300, 300, FALSE );
-#endif
+    create_GLUT_window( "Test Window", 100, 600, 300, 300, FALSE );
+    create_GLUT_window( "Second Window", 500, 600, 200, 200, TRUE );
 
     glutMainLoop();
 
