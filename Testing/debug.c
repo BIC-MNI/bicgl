d1ec61154349f7758b6b1c4358ea0bfb7acd7b67
david <david>
1998-08-20 16:28:20 +0000

    *** empty log message ***

--- Testing/debug.c
+++ Testing/debug.c
@@ -1,10 +1,10 @@
-#include  <internal_volume_io.h>
+#include  <stdio.h>
 #include  "../../GLUT/include/glut.h"
 
-private int  test_glut_window(
-    BOOLEAN   colour_map_mode,
-    BOOLEAN   double_buffer_flag,
-    BOOLEAN   depth_buffer_flag )
+static int  test_glut_window(
+    int   rgb_mode,
+    int   double_buffer_flag,
+    int   depth_buffer_flag )
 {
     int                rgba, doub, depth;
     unsigned  int      mode;
@@ -13,10 +13,10 @@ private int  test_glut_window(
 
     mode = 0;
 
-    if( colour_map_mode )
-        mode |= GLUT_INDEX;
-    else
+    if( rgb_mode )
         mode |= GLUT_RGB;
+    else
+        mode |= GLUT_INDEX;
 
     if( double_buffer_flag )
         mode |= GLUT_DOUBLE;
@@ -30,19 +30,20 @@ private int  test_glut_window(
 
     if( !glutGet( (GLenum) GLUT_DISPLAY_MODE_POSSIBLE ) )
     {
-        (void) printf( "Test window: Could not open GLUT window in Display mode (%d,%d,%d) for OpenGL\n",
-                     colour_map_mode, double_buffer_flag, depth_buffer_flag );
+        (void) printf(
+              "Test window: GLUT display mode not possible (%d,%d,%d)\n",
+              rgb_mode, double_buffer_flag, depth_buffer_flag );
         return( -1 );
     }
 
     (void) sprintf( title, "Title: %d %d %d",
-                    colour_map_mode, double_buffer_flag, depth_buffer_flag );
+                    rgb_mode, double_buffer_flag, depth_buffer_flag );
 
     window_id = glutCreateWindow( title );
 
     if( window_id < 1 )
     {
-        (void) printf( "Test window: Could not open GLUT window for OpenGL\n" );
+        (void) printf( "Test window: Could not open GLUT window\n" );
         return( -1 );
     }
 
@@ -50,69 +51,56 @@ private int  test_glut_window(
     doub = glutGet((GLenum) GLUT_WINDOW_DOUBLEBUFFER);
     depth = glutGet((GLenum) GLUT_WINDOW_DEPTH_SIZE);
 
-    if( rgba != !colour_map_mode ||
+    if( rgba != rgb_mode ||
         doub != double_buffer_flag ||
         (depth > 0) != depth_buffer_flag )
     {
-        (void) printf( "Test window: Could not get requested mode( %d %d %d ), got( %d %d %d )\n",
-                     colour_map_mode, double_buffer_flag, depth_buffer_flag,
-                     !rgba, doub, (depth > 0 ) );
+        (void) printf(
+            "Test window: Window was opened in incorrect mode ( %d %d %d ), got( %d %d %d )\n",
+            rgb_mode, double_buffer_flag, depth_buffer_flag,
+            rgba, doub, (depth > 0 ) );
     }
 
     return( window_id );
 }
 
-private  void  test_glut_windows( void )
+static  void  display_function( void )
 {
-    int  window_id;
+}
 
-    window_id = test_glut_window( FALSE, FALSE, FALSE );
-    glutDestroyWindow( window_id );
+static  void  test_glut_windows( void )
+{
+    int  i, window_ids[8];
 
-    window_id = test_glut_window( FALSE, FALSE, TRUE );
-    glutDestroyWindow( window_id );
+    window_ids[0] = test_glut_window( 0, 0, 0 );
+    window_ids[1] = test_glut_window( 0, 0, 1 );
+    window_ids[2] = test_glut_window( 0, 1, 0 );
+    window_ids[3] = test_glut_window( 0, 1, 1 );
+    window_ids[4] = test_glut_window( 1, 0, 0 );
+    window_ids[5] = test_glut_window( 1, 0, 1 );
+    window_ids[6] = test_glut_window( 1, 1, 0 );
+    window_ids[7] = test_glut_window( 1, 1, 1 );
 
-    window_id = test_glut_window( FALSE, TRUE, FALSE );
-    glutDestroyWindow( window_id );
-
-    window_id = test_glut_window( FALSE, TRUE, TRUE );
-    glutDestroyWindow( window_id );
-
-    window_id = test_glut_window( TRUE, FALSE, FALSE );
-    glutDestroyWindow( window_id );
-
-    window_id = test_glut_window( TRUE, FALSE, TRUE );
-    glutDestroyWindow( window_id );
-
-    window_id = test_glut_window( TRUE, TRUE, FALSE );
-    glutDestroyWindow( window_id );
-
-    window_id = test_glut_window( TRUE, TRUE, TRUE );
-    glutDestroyWindow( window_id );
+    for( i = 0;  i < 8;  ++i )
+    {
+        glutSetWindow( window_ids[i] );
+        glutDisplayFunc( display_function );
+    }
 }
 
 int main(
     int    argc,
     char   *argv[] )
 {
-    Status            status;
-
     glutInit( &argc, argv );
 
-    print( "\n" );
+    (void) printf( "\n1:\n" );
     test_glut_windows();
 
-    print( "\n" );
+    (void) printf( "\n2:\n" );
     test_glut_windows();
 
-    print( "\n" );
-    test_glut_windows();
-
-    print( "\n" );
-    test_glut_windows();
-
-    print( "\n" );
-    test_glut_windows();
+    /*--- glutMainLoop(); ---*/
 
     return( 0 );
 }
